486451caa376a72ae7ca0ccc24f813319b3ff700
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2024-07-11 12:33:55 +0530

    Update format_json.py (#11)

--- TOOLS/format_json.py
+++ TOOLS/format_json.py
@@ -1,20 +1,24 @@
 import json
 from pathlib import Path
 
+# Define the directory path to start searching for JSON files
 dir_path = Path(".")
 
-# Find all JSON files in the source directory and its subdirectories
+# Find all JSON files in the directory and its subdirectories
 json_files = dir_path.rglob("*.json")
+
+# Iterate over each JSON file found
 for file_path in json_files:
     try:
         # Read the JSON content from the source file
         with open(file_path, "r", encoding="utf-8") as source_file:
-            data = json.load(source_file)
+            data = json.load(source_file)  # Load the JSON data into a Python object
 
-        # Write the JSON content to the target file
+        # Write the JSON content to the target file with pretty formatting
         with open(file_path, "w", encoding="utf-8") as target_file:
-            json.dump(data, target_file, ensure_ascii=False, indent=4)
-            target_file.write("\n")
+            json.dump(data, target_file, ensure_ascii=False, indent=4)  # Dump the JSON data back to the file
+            target_file.write("\n")  # Add a newline at the end for better readability
 
     except Exception as e:
+        # Print any exception that occurs during file reading/writing
         print(e)
